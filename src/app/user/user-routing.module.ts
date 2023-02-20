b91dd61f4738b57c260b5354a51f8facb877ab67
Ayush Dhimmar <ayush.dhimmar@1rivet.local>
2023-02-20 12:03:06 +0530

    feat : routing

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListContainerComponent } from './user-list-container/user-list-container.component';
 import { UserComponent } from './user.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }];
+const routes: Routes = [
+  { path: '',
+  component: UserComponent,
+  children : [
+    {
+      path: '',
+      pathMatch: 'full',
+      redirectTo : 'user-list'
+    },
+    {
+      path : 'user-list',
+      component : UserListContainerComponent
+    }
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
