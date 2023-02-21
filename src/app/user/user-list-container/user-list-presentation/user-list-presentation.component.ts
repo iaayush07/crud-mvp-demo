8ea32e8ed176cc0b8060b5d814265c9b1f90224e
Ayush Dhimmar <ayush.dhimmar@1rivet.local>
2023-02-21 13:56:50 +0530

    feat: comment

--- src/app/user/user-list-container/user-list-presentation/user-list-presentation.component.ts
+++ src/app/user/user-list-container/user-list-presentation/user-list-presentation.component.ts
@@ -10,11 +10,17 @@ import { user } from '../../user.model';
 export class UserListPresentationComponent {
   public userList! :  user[];
 
+  /**
+   * setter for getting data
+   */
   @Input() public set userData(value: user[] | null) {
     if (value) {
       this.userList = value;
     }
   }
+  /**
+   * getter for getting data
+   */
   public get userData(): user[] {
     return this.userList;
   }
